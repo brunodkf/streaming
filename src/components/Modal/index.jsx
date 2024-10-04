107e0e767296eaa758b2ee60b6c0c0c152aab51d
Bruno Furtado <bsouzacc@gmail.com>
2024-10-04 19:02:55 -0300

    update /react player

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -6,23 +6,12 @@ import { FaWindowClose } from "react-icons/fa";
 
 const youtubeEmbed = import.meta.env.VITE_YOU_TUBE;
 
-export default function Modal({ isOpen, trailer, closeModal, what, background }) {
-
-    const [modalBanner, setModalBanner] = useState('');
-    const [modalPlayer, setModalPlayer] = useState('');
-
-    useEffect(() => {
-        if (what === 'banner') {
-            setModalBanner('hidden')
-        } else if (what === 'player') {
-            setModalPlayer('absolute top-0 bg-slate-500 w-svw h-svh')
-        }
-    }, [what])
+export default function Modal({ isOpen, trailer, closeModal, background }) {
 
     if (isOpen) {
         return (
-            <div className={`modal ${modalBanner}${modalPlayer} absolute top-0 flex flex-col items-center justify-center bg-slate-500 w-svw h-svh z-50 bg-cover bg-no-repeat bg-center`} style={{ backgroundImage: `url(${background})` }}>
-                <div className="modal__content bg-vermelho-hover w-full h-1/3 flex flex-col items-center justify-center">
+            <div className={`modal  w-svw h-svh absolute top-0 flex flex-col items-center justify-center  z-50 bg-cover bg-no-repeat bg-center before:content-[''] before:absolute before:w-svw before:h-svh before:bg-preto-before`} style={{ backgroundImage: `url(${background})` }}>
+                <div className="modal__content bg-vermelho-hover w-full h-1/3 flex flex-col items-center justify-center z-50">
                     <div className="modal__nav w-full flex items-center justify-between px-4">
                         <h2>Assista ao trailer:</h2>
                         <FaWindowClose onClick={closeModal} />
