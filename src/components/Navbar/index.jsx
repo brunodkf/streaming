76e6991174c7672c55848682a4e0c8b9bcc342ea
Bruno Furtado <bsouzacc@gmail.com>
2024-10-07 17:56:17 -0300

    update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,9 +14,9 @@ export const Navbar = () => {
             <nav className='navbar__container container m-auto p-4 flex items-center justify-between' id="navbar">
                 <img className='w-20' src={Logo} alt="Logo" />
 
-                <div className="hidden md:flex w-2/4 border-cinza-transparente border-2 rounded-xl">
+                <div className="hidden md:flex w-2/4 border-cinza-transparente border-2 rounded-xl overflow-hidden">
                     <input type="search" className='w-full bg-transparent  rounded-r-none' />
-                    <button className='bg-cinza-transparente px-3 rounded-r-xl'>
+                    <button className='bg-cinza-transparente px-3'>
                         <RiSearchLine className='text-white text-xl' />
                     </button>
                 </div>
