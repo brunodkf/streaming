57361846c1a46c8dbdd2a69a0097b9a15b6b8a84
Bruno Furtado <bsouzacc@gmail.com>
2024-11-04 13:01:45 -0300

    update /links do site

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,11 +10,11 @@ import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <section id='footer' className='footer w-screen h-screen bg-preto-claro border-t-2 border-t-cinza-escuro'>
-            <div className="container h-4/5 m-auto">
+        <section id='footer' className='footer w-screen h-screen md:h-auto bg-preto-claro md:bg-preto-escuro border-t-2 border-t-cinza-escuro'>
+            <div className="container h-4/5 md:h-auto m-auto">
                 <img className='w-20 m-auto mt-12' src={LogoBranco} alt="Logo Movie" />
 
-                <ul className=' w-full h-2/4 flex flex-col justify-between mt-10 py-4 text-center uppercase text-white tracking-wider mr-10 '>
+                <ul className=' w-full h-2/4 md:max-w-96 md:h-auto flex flex-col md:flex-row justify-between mt-10 md:m-auto md:mt-10 py-4 text-center uppercase text-white tracking-wider mr-10 '>
                     <li>
                         <Scroll to='/'>
                             Início
@@ -38,7 +38,7 @@ const Footer = () => {
                     </li>
                 </ul>
 
-                <ul className='socials w-2/3 m-auto flex items-center justify-between text-white text-3xl mt-14'>
+                <ul className='socials w-2/3 md:max-w-48 m-auto flex items-center justify-between text-white text-3xl mt-14'>
                     <li>
                         <Link to='/'>
                             <FaInstagram />
@@ -62,7 +62,9 @@ const Footer = () => {
                 </ul>
 
 
-                <p className='mt-20 text-center text-white'>Desenvolvido por <br /> <a href="">@brunodkf</a></p>
+                <p className='mt-20 text-center text-white md:pb-20'>© Copyright 2024 Todos os direitos reservados | Developed by <a href="">@brunodkf</a></p>
+
+                
             </div>
         </section>
     )
